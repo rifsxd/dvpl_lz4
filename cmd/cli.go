4ca3147dc1f9301c771a15b16039d60ec96660a6
Rifat Azad <33044977+rifsxd@users.noreply.github.com>
2024-03-07 08:11:57 +0600

    updated commit date

--- cmd/cli.go
+++ cmd/cli.go
@@ -51,7 +51,7 @@ const Name = "DVPL_LZ4 CLI TOOL"
 const Version = "1.2.0"
 const Repo = "https://github.com/rifsxd/dvpl_lz4"
 const Web = "https://rxd-mods.xyz"
-const Commit = "06/03/2024"
+const Commit = "07/03/2024"
 const Info = "A CLI/GUI Tool Coded In GoLang To Convert WoTB ( Dava ) SmartDLC DVPL File Based On LZ4 High Compression."
 
 func Cli() {
